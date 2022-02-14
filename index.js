4e7f862a5aeb715ec3b1f344dba3f6dc1b254b8a
ZiaulhaqHaqyar <fnu.ziaulhaq@flatironschool.com>
2022-02-13 22:54:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+//const { doc } = require("mocha/lib/reporters");
+
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+
+  function moveDodgerRight() {
+      const rightNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(rightNumbers, 0);
+
+      if (left > 20) {
+          dodger.style.left = `${left + 1}px`
+      }
+  }
+document.addEventListener("keyup", function (e) {
+    if (e.key === "rightArrow") {
+        moveDodgerRight();
+    }
+})
